7566e5c1bdc39f31622350064cb46bc0437f323a
anton <1288stef1288@gmail.com>
2025-03-30 16:31:42 +0300

    <landing_page_b-o>

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const html = document.querySelector('html');
+
+function handleClick() {
+  html.classList.toggle('unscroll');
+}
+
+document.querySelector('.icon--burger').addEventListener('click', handleClick);
+document.querySelector('.icon--close').addEventListener('click', handleClick);
+document.querySelector('.nav__list').addEventListener('click', handleClick);
+document.querySelector('.aside-scroll').addEventListener('click', handleClick);
